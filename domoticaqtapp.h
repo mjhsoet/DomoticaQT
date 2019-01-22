069200be0a7ac969515a492b6da820e572128fc9
Unknown <mjh.soet@student.avans.nl>
2019-01-22 13:58:52 +0100

    Created basic app with basic functionalities

--- domoticaqtapp.h
+++ domoticaqtapp.h
@@ -2,6 +2,8 @@
 #define DOMOTICAQTAPP_H
 
 #include <QWidget>
+#include <QListView>
+#include <QListWidgetItem>
 
 namespace Ui {
 class DomoticaQTApp;
@@ -15,8 +17,27 @@ public:
     explicit DomoticaQTApp(QWidget *parent = nullptr);
     ~DomoticaQTApp();
 
+public:
+    void setButtonAssignmentList();
+    void setAssignment();
+
+private slots:
+    void on_nodeList_itemClicked(QListWidgetItem *selectedNode);
+
+    void on_redLed_clicked();
+
+    void on_greenLed_clicked();
+
+    void on_buttonAssign_clicked();
+
+    void on_buttonSelectList_itemDoubleClicked(QListWidgetItem *selectedOption);
+
 private:
     Ui::DomoticaQTApp *ui;
+    bool nodeSelected;
+    QString selectedNode, nodeClicked;
+    int nodeAmmount;
+    int list;
 };
 
 #endif // DOMOTICAQTAPP_H
